4b68c223d42d803d56671557c3f436179d5ebba8
Ahn <gan_jinan@qq.com>
2019-07-11 21:14:53 +0800

    feat: practice06 add the person, student and teacher information and finish the test.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (this.getKlass() == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + String.valueOf(this.getKlass()) + ".";
+        }
+    }
 }
