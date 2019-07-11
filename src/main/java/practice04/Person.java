c5c904060aea87e47fac181e9df1fff747224d15
Ahn <gan_jinan@qq.com>
2019-07-11 20:45:28 +0800

    feat: practice04 add the person, student and worker information and finish the test.

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,29 @@
 package practice04;
 
 public class Person {
+
+    private String name;
+
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String introduce () {
+        return "My name is " + this.name + ". I am " + String.valueOf(this.age) + "years old.";
+    }
+
+    public String basicIntroduce () {
+        return "My name is " + this.name + ". I am " + String.valueOf(this.age) + "years old.";
+    }
 }
